c321b79fccce1a5b79f82a8b1cbaeebebddedcab
leosunz <devsunleez@gmail.com>
2025-09-23 07:08:42 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    // Add this if you're deploying to a subpath
+    // base: '/your-repo-name/'
+  },
   plugins: [react()],
 })
